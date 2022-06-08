c4cb94bea604068a3afc439b4ab03a98b8494549
Wezley Singleton <wezley.singleton@gmail.com>
2022-06-08 11:01:42 -0400

    quick bootstrap demo showing off its grid system

--- web/js/js-intro.js
+++ web/js/js-intro.js
@@ -264,3 +264,186 @@ console.log(defaultTruck);
 let myTruck = new Truck('Ford', 'Ranger', 'Red', '2001');
 console.log(myTruck);
 myTruck.honk();
+
+//------------------------------------
+
+// ES = ECMA (European Computer Manufacturers Association) Script
+// ECMA -> the standard that the JS language is based upon
+
+// ES6 = ECMA Script 2015
+// ES7 = ECMA Script 2016
+// ...
+// ES10 = ECMA Script 2019
+
+// Most JS applications that will run in the browser will be transpiled into ES5 (since ES5 is 100% supported by all modern browsers)
+// Transpilation = converting high-level syntax into another syntax that is equally high-level (important when we get into TypeScript)
+
+//------------------------------------
+
+// Class syntax (syntactic sugar - no functional difference, just syntactically more convenient)
+class Cat {
+
+    // A JS class can only have 1 constructor
+    constructor(name, furColor, breed = 'Domestic Shorthair') {
+        this.name = name;
+        this.furColor = furColor;
+        this.breed = breed;
+    }
+
+    makeSound() {
+        console.log('Meow!');
+    }
+
+}
+
+let someCat = new Cat('Paul', 'black');
+console.log(someCat);
+console.log(someCat.makeSound());
+
+//-------------------------------------------
+
+/*
+    Type Coercion and Truthy/Falsy Values
+
+    Falsy Values:
+        - ''/"" (empty string)
+        - 0, -0, +0, 0n
+        - null
+        - undefined
+        - false
+        - NaN
+ */
+
+console.log(7 + '7' + 7); // '777'
+console.log(7 + 7 + '7'); // '147'
+console.log(7 == '7'); // true - loose comparison (type coercion will be applied)
+console.log(7 === '7'); // false - strict comparison (no type coercion)
+
+let username = 'wsingleton';
+let password = 'password';
+
+if (username != null && username != '') {
+    console.log('username is not null or empty. aka truthy!');  
+}
+
+if (password) {
+    console.log('password is truthy!');
+}
+
+function login(un, pw) {
+    console.log('username: ' + un);
+    console.log('password: ' + pw);
+}
+
+// guard operator
+(username && password) && login(username, password);
+
+// equivalent of the above
+// if (username && password) {
+//     login(username, password)
+// }
+
+// X || Y
+
+// default operator
+function returnSomeValue(x) {
+    return x || 'default-value';
+}
+
+console.log(returnSomeValue(0));
+
+console.log(100 / 0); // infinity
+console.log(100 / 'hello'); // NaN
+console.log(typeof(100)); // number
+console.log(typeof(NaN)); // NaN (not a number) is a number
+
+let a = 100 / 'hello';
+let b = a;
+console.log(a == b); // actually false, and not because of JS (IEEE defines NaN == NaN as false always)
+
+//-------------------------------
+
+// Aggregate structures (arrays, maps, sets, etc.)
+let numbers = [1, 2, 3, 4, 5];
+let myArray = new Array(16); // creates an array with 16 empty values within it
+console.log(numbers);
+console.log(myArray);
+
+// Arrays in JS are dynamically sizeable!
+numbers.push(6);
+console.log(numbers);
+
+// Access to values within an array is the same as it is in Java
+console.log(numbers[0]); // grabs the first thing
+console.log(numbers[numbers.length - 1]); // grabs the last thing
+
+function isEven(num) {
+    return num % 2 == 0;
+}
+
+let evenNumbers = numbers.filter(isEven);
+console.log(evenNumbers);
+
+let oddNumbers = numbers.filter(function(num) {
+    return num % 2 != 0;
+});
+console.log(oddNumbers);
+
+let timesTenValues = numbers.map(num => num * 10);
+console.log(timesTenValues);
+
+let sum = timesTenValues.reduce((accumulator, currentVal) => accumulator + currentVal);
+console.log(sum);
+
+// Note that the original array is unchanged
+console.log(numbers);
+
+// Maps
+let someMap = new Map();
+someMap.set('wsingleton', {firstName: 'Wezley', lastName: 'Singleton'});
+someMap.set('tester99', {firstName: 'Tester', lastName: 'McTesterson'});
+someMap.set('wsingleton', {firstName: 'Replaced', lastName: 'Original'});
+
+console.log(someMap.get('wsingleton'));
+
+//-------------------------
+
+// looping constructs
+
+// basically the same as Java (minus the use of let)
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log('+------------------------------+');
+
+// for..of loop (iterates over iterable objects [strings, arrays, maps, etc.])
+for (let value of numbers) {
+    console.log(typeof(value)); // number
+    console.log(value); // prints as a number
+}
+
+console.log('+------------------------------+');
+// for..in loop (used iterate over the keys of some object) (not typical to use with arrays)
+for (let value in numbers) {
+    console.log(typeof(value)); // string
+    console.log(value); // prints as a string, rather than a number
+}
+
+let me = {
+    'firstName': 'Wezley', // legal, but not really required (makes no difference)
+    lastName: 'Singleton',
+    email: 'wezley.singleton@revature.com',
+    username: 'wsingleton',
+    '1a': 'but why?',
+    'more common': 'you\'ll see this from time to time'
+}
+
+console.log(me.username); // access object properties using dot notation
+console.log(me["username"]); 
+console.log(me["more common"]); // access object properties using bracket notation
+
+// to iterate over the keys of some object and see their values
+for (let prop in me) {
+    console.log(`${prop}: ${me[prop]}`);
+}
